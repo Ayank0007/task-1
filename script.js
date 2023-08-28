46e2cac37168c09f7353c617decbeafdfbd790cc
Ayank0007 <ayankhan473551@gmail.com>
2023-08-28 18:31:02 +0530

    Adding Files

---
+++ script.js
@@ -0,0 +1,12 @@
+const celsiusInput = document.getElementById("celsius");
+const fahrenheitInput = document.getElementById("fahrenheit");
+celsiusInput.addEventListener("input", () => {
+  const celsius = parseFloat(celsiusInput.value);
+  const fahrenheit = (celsius * 9 / 5) + 32;
+  fahrenheitInput.value = fahrenheit.toFixed(2);
+});
+fahrenheitInput.addEventListener("input", () => {
+  const fahrenheit = parseFloat(fahrenheitInput.value);
+  const celsius = (fahrenheit - 32) * 5 / 9;
+  celsiusInput.value = celsius.toFixed(2);
+});
